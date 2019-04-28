eed05543af331874fdbd0e33f4ec15d2d541b0b9
jaredjames2020 <40965328+jaredjames2020@users.noreply.github.com>
2019-04-28 13:45:32 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'jj2020'
+LOGGER = getLogger(__name__)
+
+class GoodMorningSkill(MycroftSkill):
+
+  def __init__(self):
+    super(GoodMorningSkill, self).__init__(name="GoodMorningSkill")
+    
+  def handle_GoodMorning_intent(self, message):
+    self.speak_dialog("goodmorning")
+    
+def create_skill():
+  return GoodMorningSkill()
